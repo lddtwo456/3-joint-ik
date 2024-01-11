435bdfe281a59ae3f891bbdbbdde721336607dac
lddtwo456 <72422028+lddtwo456@users.noreply.github.com>
2024-01-11 11:23:56 -0500

    can invert

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ WIN = pygame.display.set_mode((800, 500))
 
 scale = 20
 
-clicked = False
+inverted = False
 
 while True:
     WIN.fill((0,0,0))
@@ -32,7 +32,13 @@ while True:
             pygame.quit()
             sys.exit()
 
-    # ROTATE TARGET ANGLE WITH ARROW KEYS
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                if inverted:
+                    inverted = False
+                else:
+                    inverted = True
+    
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
         theta3+=0.0025
@@ -46,12 +52,27 @@ while True:
 
     p2[0] = p3[0] - (l3*np.cos(theta3))
     p2[1] = p3[1] - (l3*np.sin(theta3))
-
+    
     theta2 = np.arccos(((p2[0]*p2[0]) + (p2[1]*p2[1]) - (l1*l1) - (l2*l2))/(2*l1*l2))
     theta1 = np.arctan2(p2[1], p2[0]) - np.arctan2((l2 * np.sin(theta2)), (l1 + l2 * np.cos(theta2)))
 
     p1[0] = l1*np.cos(theta1)
     p1[1] = l1*np.sin(theta1)
+
+    if inverted:
+        print("inverted")
+        mid_p = [(p0[0] + p2[0]) / 2, (p0[1] + p2[1]) / 2]
+
+        pygame.draw.circle(WIN, (155, 155, 155), (p1[0]*scale + 400, p1[1]*scale*-1 + 250), 3)
+
+        p1[0] -= mid_p[0]
+        p1[1] -= mid_p[1]
+
+        p1[0] *= -1
+        p1[1] *= -1
+
+        p1[0] += mid_p[0]
+        p1[1] += mid_p[1]
 
     print(f"{np.rad2deg(theta1)} {np.rad2deg(theta2)}")
 
